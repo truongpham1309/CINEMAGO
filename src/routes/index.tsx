40329a937b9638a9cdb6857533da865c2bf878b0
dainm2234 <dainmph30444@fpt.edu.vn>
2024-05-22 09:34:22 +0700

    feat 404 Not Found UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import SignUpUserPage from "@/pages/auth/SignUp/page"
 import LayoutDashBoard from "@/pages/dashboard/LayoutDashBoard"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import PrivateRouterLogin from "./PrivateRouterLogin"
+import NotFoundPage from "@/pages/website/404/page"
 
 const Router = () => {
   return (
@@ -23,6 +24,8 @@ const Router = () => {
             <Route path="dashboard" element={<LayoutDashBoard />}>
               <Route index element={<>DashBoard Home</>} />
             </Route>
+
+            <Route path="/404"  element={<NotFoundPage/>}/>
         </Routes>
       </BrowserRouter>
     </>
